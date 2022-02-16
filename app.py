68fcf2a69dca825744e93bf5e347cb9e820e5694
Deusie <43952614+Deusie@users.noreply.github.com>
2022-02-16 11:45:13 +0100

    Add Html templates, Pandas import, eigen bestand met 2 methodes

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from bestandjeJesse import *
 
 app = Flask(__name__)
 
-@app.route("/<username>")
-def hello_world(username):
-    return f"<p>Hello,{username} World!</p>"
+@app.route("/")
+def index():    
+    return render_template("index.html")
+
+@app.route("/jesse")
+def methodeEen():
+    terug = eenmethodeinbestand1()
+    return "methodeeen"+terug
+
+@app.route("/twee")
+def methodeTwee():
+    terug = methodeMetReadFile()
+    return "methodeEen "+terug
+
+# @app.route("/metinput/<username>")
+# def hello_world(username):
+#     return f"<p>Hello,{username} World!</p>"
